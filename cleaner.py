ea3a53fd892658c3b66fa8fa22f1bd1e60b71fc9
Chad Ternes <chad.m.ternes@gmail.com>
2023-07-18 16:36:09 -0600

    adding new files and update cleaner.py to most recent

--- cleaner.py
+++ cleaner.py
@@ -1,10 +1,34 @@
 import re
 
-p = re.compile('\w+\/?(\w+)?(\.)?(\w+)?')
+def classification_type_check(string):
+    return string[0].isalpha()
 
-x = input("Enter classifications: ")
+def convert_string_to_list(string):
+    if classification_type_check(string) == True:
+        ICPclass_list = re.findall(r'\w+(?:\/)?(?:\w+)?', input)
+        return ICPclass_list
+    else:
+        USclass_list = re.findall(r'\d+(?:\/)?(?:\w+)?(?:\.)?(?:\w+)?', input)
+        return USclass_list
 
-p.match(x)
+def remove_US_dots_letters(list):
+    letter_format = re.compile(r'(\d+\/\d+)[a-zA-Z]')
+    dot_format = re.compile(r'(\d+\/\d+)\.\w+')
+    for i in USclass_list:
+        if i == letter_format or dot_format:
+            print("format")
+
+input = "435; 435/462; 21/27F 424/164.1; 424/164.100P;"
+
+if classification_type_check(input) == False:
+    remove_US_dots_letters(classification_type_check(input))
+    
+
+
+print(convert_string_to_list(input))
+
+
+
 
 
 
