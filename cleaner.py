5272efef53c5febf6dd84b0608846ca1210953c3
Chad Ternes <chad.m.ternes@gmail.com>
2023-07-19 16:33:56 -0600

    update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -11,31 +11,21 @@ def convert_string_to_list(string):
         return class_list
     else:
         USclass_list = re.findall(r'\d+\/?\d+\.?\w+', input)
-        US_pattern_decimal = re.compile(r'(\d+)\/(\d+)(?:\w+|\.)(?:\w+)?')
+        US_pattern_decimal = re.compile(r'(\d+)\/(\d+)(?:[a-zA-z]+|\.)(?:\w+)?')
         class_list = [re.sub(US_pattern_decimal, r'\1/\2*', x) for x in USclass_list] #this removes any decimals and/or trailing letters 
         return class_list                                                             #and replaces with an asterisk
         
         
+#input = "435; 435/462; 21/27F 424/164.1; 424/164.100P;"
+input = "A61K; C07K19/00; C12N15/28; C12N15/28"
 
-#def remove_US_dots_letters(list):
-    #letter_format = re.compile(r'(\d+\/\d+)[a-zA-Z]')
-    #dot_format = re.compile(r'(\d+\/\d+)\.\w+')
-    #for i in USclass_list:
-        #if i == letter_format or dot_format:
-            #print("format")
-
-US_pattern = re.compile(r'(\d+)(?:\/)?(\w+)?\.(\w+)?')
+list = convert_string_to_list(input)
+unique_list = []
+[unique_list.append(x) for x in list if x not in unique_list]
+print(*unique_list, sep=' or ')
 
 
-input = "435; 435/462; 21/27F 424/164.1; 424/164.100P;"
-#input = "A61K; C07K19/00; C12N15/28; C12N15/28"
 
-#if classification_type_check(input) == False:
-    #remove_US_dots_letters(classification_type_check(input))
-    
-#class_list = convert_string_to_list(input))
-
-print(convert_string_to_list(input))
 
 
 
